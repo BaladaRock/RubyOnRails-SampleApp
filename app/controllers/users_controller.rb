940b6f35ed28cf8ef503124a439309a3e8d857af
Andrei Daian <andreidaian05@gmail.com>
2020-11-20 14:28:06 +0200

    Added gravatar profilepage + aside content(empty for now) for 'index/users'. Added Sig-up form using form_with method. (at '/new' request). Started creating data validation while inserting input data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,22 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    #also works with 'byebug' command
-    #debugger - used with gem byebug, to initiate debug mode
+    #- used with gem byebug, to initiate debug mode
+    # also works with 'byebug' command
+    #debugger 
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user]) # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
   end
   
 end
