f3a1b0ca4229cb0a2d23f250ba8cadfc01f0da57
Andrei Daian <andreidaian05@gmail.com>
2020-11-23 18:47:09 +0200

    Added automatically loggin in after user sign up. Added is_log_in? method inside test_helper class(for int test)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    #debugger 
     #- used with gem byebug, to initiate debug mode
     # also works with 'byebug' command
-    #debugger 
   end
 
   def new
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
