466fbced1a3b3e11d50ed4e1018d796b408ad5a9
Andrei Daian <andreidaian05@gmail.com>
2020-11-23 22:52:41 +0200

    Added logout option via destroy controller. Added log_out helper method and is_logged_in helper for tests. Added test to check the state after loggin in + logging out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -30,25 +30,60 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
 
   test "login with valid email/invalid password" do
-    # Send a request to correct path
+    # Send a request for login_page
     get login_path
 
-    # Post a valid session hash(using @user that was created by the help 
-    # of users.yml fixtures class)
-    post login_path, params: { session: { email: 'example@railstutorial.org',
+    # Check that the received template is the login_page content
+    assert_template 'sessions/new'
+
+    # Fill in the form with correct email and incorrect password
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
+      
+    # Certify that no user logged in                                      
+    assert_not is_logged_in?
+
+    # Recheck for login page template
+    assert_template 'sessions/new'
+
+    # Flash warning should be present
+    assert_not flash.empty?
+
+    # Flash warning should dissapear after sending another request
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do
+    # Send a request for login_page
+    get login_path
+
+    # Fill in the form with correct email and password
+    post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
-
-    # Check the redirect target                                 
+    
+    # User should be logged in  and browser should automatically
+    # be redirected to its profile page                                   
+    assert is_logged_in?
     assert_redirected_to @user
-
-    #Visit the target page
     follow_redirect!
 
-    #Check for the wanted layout
+    # Check for the wanted layout elements
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+
+    # After that, log out and refirect to home_page
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+
+    #Layout elements should get back to normal, no-login state
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
   
 end
