21b4917cb08e2ba983dd4635599c76e178d9c2ca
Andrei Daian <andreidaian05@gmail.com>
2020-11-25 23:43:04 +0200

    Fixed checking box tests(switched assert_null to assert_empty, for cookies['remember_token']

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -17,5 +18,19 @@ class ActiveSupport::TestCase
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+   # Log in as a particular User.
+   def log_in_as(user)
+    session[:user_id] = user.id
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  # Log in as a particular User.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
