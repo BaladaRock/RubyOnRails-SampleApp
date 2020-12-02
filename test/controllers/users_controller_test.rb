e19aa0e95560bd5fa5849fc1eeeca949cb27c27d
Andrei Daian <andreidaian05@gmail.com>
2020-12-02 02:37:58 +0200

    Finished CRUD operations for users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -55,4 +55,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_not @second_user.reload.admin?
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@first_user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@second_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@first_user)
+    end
+    assert_redirected_to root_url
+  end
+
 end
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:night_bot)
+    @admin = users(:night_bot)
+    @non_admin = users(:archer)
   end
 
-  test "index including pagination" do
+  test "index as admin including pagination and delete links" do
     # Login user
-    log_in_as @user
+    log_in_as @admin
 
     # Send request for users list page
     get users_path
@@ -23,6 +24,30 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
     # Assure that both of the paginations are displayed
     assert_select 'div.pagination', count: 2
-
+    
+    # Get the first page of users and check that 'delete' option
+    # is available for every non-admin ones
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    
+    # Check that deleting one non-admin user has worked
+    assert_difference 'User.count', -1 do
+    delete user_path(@non_admin)
+    end
   end
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+
+    # Non-admin users should not be able to delete other users
+    assert_select 'a', text: 'delete', count: 0
+  end
+
+
 end
