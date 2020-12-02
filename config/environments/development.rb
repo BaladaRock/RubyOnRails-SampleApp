2b920b35117e972df697a8fb4d0be0dd4d9b4f17
Andrei Daian <andreidaian05@gmail.com>
2020-12-02 21:34:24 +0200

    Finished layouts for account activation. Added email previews using localhost + test for account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,6 +36,13 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  host = 'localhost:3000'       # Local server
+  # Use this on the cloud IDE.
+  #config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+
+  # Use this if developing on localhost.
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
