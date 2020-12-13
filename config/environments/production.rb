6dce1dd05c67ea3f8b1492d1e4114394fa32041e
Andrei Daian <andreidaian05@gmail.com>
2020-12-13 13:33:52 +0200

    Fixed host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_production"
 
-  config.action_mailer.default_url_options = { :host => 'intense-basin-67101.heroku.com' }  
+  config.action_mailer.default_url_options = { :host => 'https://intense-basin-67101.herokuapp.com' }  
   config.action_mailer.delivery_method = :smtp  
   config.action_mailer.perform_deliveries = true  
   config.action_mailer.raise_delivery_errors = false  
