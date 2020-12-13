09cdeb2c284848b3f194c6ed280ec9f16d0dcfea
Andrei Daian <andreidaian05@gmail.com>
2020-12-13 13:43:34 +0200

    Also corrected domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {  
     address: "smtp.gmail.com",
     port: 587,
-    domain: "intense-basin-67101.heroku.com",
+    domain: "https://intense-basin-67101.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
